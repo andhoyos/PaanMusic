3466822f7f148c1fddb97cb97d1ea2f336c51e38
Andres Hoyos <andresfabianhoyos@gmail.com>
2022-09-19 20:17:07 -0300

    update

---
+++ server/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+
+// function pigma_2(array, n) {
+//   for (i = 0; i < array.length; i++) {
+//     for (j = i + 1; j < array.length; j++) {
+//       let suma = array[i] + array[j];
+//       console.log(suma);
+
+//       if (array[i] + array[j] === n) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+// console.log(pigma_2([4, 5, 3, 1, 0], 8));
